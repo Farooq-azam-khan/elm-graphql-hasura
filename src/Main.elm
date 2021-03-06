8ec0c88187fbdb8cf39a7217cc90d076862afadf
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-03-06 10:29:21 -0500

    imported graphql and api types

--- src/Main.elm
+++ src/Main.elm
@@ -6,3 +6,16 @@ import Graph.OptionalArgument as OptionalArgument exposing (OptionalArgument(..)
 import GraphQLClient exposing (makeGraphQLQuery)
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
+import Graphql.OptionalArgument as OptionalArgument exposing (OptionalArgument(..))
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
+import Hasura.Enum.Order_by exposing (Order_by(..))
+import Hasura.InputObject
+    exposing
+        ( Boolean_comparison_exp
+        , Todos_bool_exp
+        , Todos_order_by
+        , buildBoolean_comparison_exp
+        , buildTodos_bool_exp
+        , buildTodos_order_by
+        )
+import Hasura.Query exposing (OnlineUsersOptionalArguments, TodosOptionalArguments)
