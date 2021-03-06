c9a15aa96af80ac5c22ce957f431adf24982cb60
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-03-06 12:15:29 -0500

    Create Constants.elm

---
+++ src/Constants.elm
@@ -0,0 +1,17 @@
+module Constants exposing (..)
+
+failMsg : String
+failMsg =
+    "Something went wrong, check network tab for details"
+
+
+signup_url : String
+signup_url =
+    "https://hasura.io/learn/auth/signup"
+
+
+login_url : String
+login_url =
+    "https://hasura.io/learn/auth/login"
+
+ 
