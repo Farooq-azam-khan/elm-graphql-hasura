c9565faa2d6f2b74dc063e48f3df450da1e9fd9d
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-03-06 10:29:40 -0500

    application login

--- src/Main.elm
+++ src/Main.elm
@@ -19,3 +19,73 @@ import Hasura.InputObject
         , buildTodos_order_by
         )
 import Hasura.Query exposing (OnlineUsersOptionalArguments, TodosOptionalArguments)
+-- Application Logic
+
+
+orderByCreatedAt : Order_by -> OptionalArgument (List Todos_order_by)
+orderByCreatedAt order =
+    Present <| [ buildTodos_order_by (\args -> { args | created_at = OptionalArgument.Present order }) ]
+
+
+equalToBoolean : Bool -> OptionalArgument Boolean_comparison_exp
+equalToBoolean isPublic =
+    Present <| buildBoolean_comparison_exp (\args -> { args | eq_ = OptionalArgument.Present isPublic })
+
+
+whereIsPublic : Bool -> OptionalArgument Todos_bool_exp
+whereIsPublic isPublic =
+    Present <| buildTodos_bool_exp (\args -> { args | is_public = equalToBoolean isPublic })
+
+
+todoListOptionalArgument : TodosOptionalArguments -> TodosOptionalArguments
+todoListOptionalArgument optionalArgs =
+    { optionalArgs | where_ = whereIsPublic False, order_by = orderByCreatedAt Desc }
+
+
+selectUser : SelectionSet User Hasura.Object.Users
+selectUser =
+    SelectionSet.map User Users.name
+
+
+todoListSelection : SelectionSet Todo Hasura.Object.Todo
+todoListSelection =
+    SelectionSet.map5 Todo
+        Todos.id
+        Todos.user_id
+        Todos.is_completed
+        Todos.title
+        (Todos.user selctUser)
+
+
+fetchPrivateTodosQuery : SelectionSet Todos RootQuery
+fetchPrivateTodosQuery =
+    Query.todos todoListOptionalArgument todoListOptionalArgument
+
+
+fetchPrivateTodos : String -> Cmd Msg
+fetchPrivateTodos authToken =
+    makeGraphQLQuery authToken
+        fetchPrivateTodosQuery
+        (RemoteData.fromResult >> fetchPrivateDataSuccess)
+
+
+type alias OnlineUser =
+    { id : String, user : User }
+
+
+type alias TodoData =
+    RemoteData (Graphql.Http.Error Todos) Todos
+
+
+type alias PrivateTodo =
+    { todos : TodoData
+    , visibility : String
+    , newTodo : String
+    }
+
+
+type DisplayForm
+    = Login
+    | Signup
+
+
